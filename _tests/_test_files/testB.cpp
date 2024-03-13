e0a9b65e85d6a29ef4c13b0dd51737be29876d70
Phuoc Tu <aptu27@gmail.com>
2024-03-12 23:48:23 -0700

    added test for init array, the allocate_twod function, init_array function, init_debug function, array_size function, print_array, and  print_twod function

--- _tests/_test_files/testB.cpp
+++ _tests/_test_files/testB.cpp
@@ -1,6 +1,10 @@
 #include "gtest/gtest.h"
 #include <iostream>
 #include <iomanip>
+#include "..\..\includes\two_d_functions\two_d_functions.h"
+#include "..\..\includes\array_functions\array_functions.h"
+#include "..\..\includes\plane\plane.h"
+#include "..\..\includes\lab\lab.h"
 using namespace std;
 
 bool test_stub(bool debug = false)
@@ -11,14 +15,170 @@ bool test_stub(bool debug = false)
   return true;
 }
 
-TEST(TEST_STUB, TestStub) {
+bool test_init_td(bool debug = false){
+  int sizes[] = {6,5,3,3,4,-1};
+  int** twod_arr = allocate_twod <int> (sizes);
   
-  //EXPECT_EQ(0, <your individual test functions are called here>);
+  
+  
+  //filling the array with -1
+  init_twod(twod_arr,sizes, -1);
+  if(debug){
+    cout << "Array filled with -1:" << endl;
+    print_twod(twod_arr,sizes);
+  }
 
-  EXPECT_EQ(1, test_stub(false));
+  if(1){
+    int** row_w = twod_arr;
+    int* col_w;
+    int* size_p = sizes;
+    int row_counter = 0;
+    while(*row_w != nullptr){
+      col_w = *row_w;
+      for(int i =0; i<*size_p;i++){
+        if(*col_w != -1){
+          cout << "FAILED: Expected to find -1 but instead found " << *col_w << " at row " << row_counter << ", column " << i << endl;
+          return false;
+        }
+        col_w++;
+      }
+      row_w++;
+      row_counter++;
+      size_p++;
+    }
+  }
+  
+  //running init_debug
+  if(debug){
+    init_twod_debug(twod_arr,sizes);
+    cout << "Init_twod_debug array:" << endl;
+    print_twod(twod_arr,sizes);
+  }
+
+
+
+  //filling array with 13
+  init_twod(twod_arr,sizes,13);
+  if(debug){
+    cout << "array filled with 13: " << endl;
+    print_twod(twod_arr,sizes);
+  }
+
+  if(1){
+    int** row_w = twod_arr;
+    int* col_w;
+    int* size_p = sizes;
+    int row_counter = 0;
+    while(*row_w != nullptr){
+      col_w = *row_w;
+      for(int i =0; i<*size_p;i++){
+        if(*col_w != 13){
+          cout << "FAILED: Expected to find 13 but instead found " << *col_w << " at row " << row_counter << ", column " << i << endl;
+          return false;
+        }
+        col_w++;
+      }
+      row_w++;
+      row_counter++;
+      size_p++;
+    }
+  }
+  
+  //fill array with default T (int) value
+  init_twod(twod_arr,sizes);
+  if(debug){
+    cout << "array filled with default T value: " << endl;
+    print_twod(twod_arr,sizes);
+  }
+
+  if(1){
+    int** row_w = twod_arr;
+    int* col_w;
+    int* size_p = sizes;
+    int row_counter = 0;
+    while(*row_w != nullptr){
+      col_w = *row_w;
+      for(int i =0; i<*size_p;i++){
+        if(*col_w != 0){
+          cout << "FAILED: Expected to find 0 but instead found " << *col_w << " at row " << row_counter << ", column " << i << endl;
+          return false;
+        }
+        col_w++;
+      }
+      row_w++;
+      row_counter++;
+      size_p++;
+    }
+  }
+
+  //----------------------//
+  //testing char array
+  char** char_twod_arr = allocate_twod <char> (sizes);
+  //fill array with default T (int) value
+  init_twod(char_twod_arr,sizes);
+  if(debug){
+    cout << "array filled with default T value: " << endl;
+    print_twod(char_twod_arr,sizes);
+  }
+
+  if(1){
+    char** row_w = char_twod_arr;
+    char* col_w;
+    int* size_p = sizes;
+    int row_counter = 0;
+    while(*row_w != nullptr){
+      col_w = *row_w;
+      for(int i =0; i<*size_p;i++){
+        if(*col_w != 0){
+          cout << "FAILED: Expected to find blank (0 in char) but instead found " << *col_w << " at row " << row_counter << ", column " << i << endl;
+          return false;
+        }
+        col_w++;
+      }
+      row_w++;
+      row_counter++;
+      size_p++;
+    }
+  }
+
+  //filling the array with 'c'
+  init_twod(char_twod_arr,sizes,'c');
+  if(debug){
+    cout << "array filled with default T value: " << endl;
+    print_twod(char_twod_arr,sizes);
+  }
+
+  if(1){
+    char** row_w = char_twod_arr;
+    char* col_w;
+    int* size_p = sizes;
+    int row_counter = 0;
+    while(*row_w != nullptr){
+      col_w = *row_w;
+      for(int i =0; i<*size_p;i++){
+        if(*col_w != 'c'){
+          cout << "FAILED: Expected to find 'c' but instead found " << *col_w << " at row " << row_counter << ", column " << i << endl;
+          return false;
+        }
+        col_w++;
+      }
+      row_w++;
+      row_counter++;
+      size_p++;
+    }
+  }
+  return true;
 }
 
 
+TEST(TEST_STUB, TestStub) { 
+  //EXPECT_EQ(0, <your individual test functions are called here>)
+  EXPECT_EQ(1, test_stub(false));
+}
+
+TEST(TEST_INIT,TestInit){
+  EXPECT_EQ(1,test_init_td(false));
+}
 
 
 int main(int argc, char **argv) {
--- includes/two_d_functions/two_d_functions.h
+++ includes/two_d_functions/two_d_functions.h
@@ -2,6 +2,7 @@
 #define TWO_D_FUNCTIONS
 #include <iostream>
 #include <fstream>
+#include "../../includes/array_functions/array_functions.h"
 
 using namespace std;
 
@@ -42,15 +43,35 @@ T* search_twod(T** twod, int* sizes, const T& key);
 template <class T>
 ostream& print_twod(T** twod, int* sizes, ostream& outs = cout);
 
+template<class T>
+void init_twod_debug(T** twod, int* sizes);
+
 
 //=======================================================================
 
 
 int array_size(int* sizes){
-
+  int counter = 0;
+  const bool debug = true;
+  while(*sizes != -1){
+    counter++;
+    sizes++;
+  }
+  if(debug){
+    cout << "# of row is: " << counter << endl;
+  }
+  return counter;
 }
 void print_array(int* a){
-
+  int* a_p = a;
+  int how_many = array_size(a_p) + 1;
+  for (int i = 0; i<how_many; i++){
+    cout << setw(6)  << *a_p << setw(6);
+    a_p++;
+    if(!(i + 1 == how_many)){
+      cout << "|";
+    }
+  }
 }
 bool index_is_valid(int* sizes, int row, int col){
 
@@ -62,7 +83,35 @@ bool index_is_valid(int* sizes, int row, int col){
 
 template <class T>
 T** allocate_twod(int* sizes){
+  assert(!(sizes == nullptr));
+  int* column_walker = sizes;
+  T** arr;
+  T** arr_walker;
 
+  //--------debug var------//
+  const bool debug = false;
+  int count_row = 0;
+  //-----------------------//
+
+  int row = array_size(sizes);
+  arr = new T* [row+1];
+  arr_walker = arr;
+  while(1){
+    if(*column_walker == -1)
+    {
+      *arr_walker = nullptr;
+      break;
+    } 
+    if(debug){
+      cout << "creating an array of " << *column_walker << " inside row of " << count_row << endl;
+    }
+    // *arr_walker = new T [*column_walker];
+    *arr_walker = allocate<T>(*column_walker);
+    count_row++;
+    column_walker++;
+    arr_walker++;
+  }
+  return arr;
 }
 
 template <class T>
@@ -87,7 +136,29 @@ T& get_twod(T** twod, int row, int col){
 
 template<class T>
 void init_twod(T** twod, int* sizes, T init_item){
-
+  assert(!(sizes == nullptr));
+  assert(!(twod == nullptr));
+  T** row_w = twod;
+  T*col_w;
+  int*sizes_p = sizes;
+  const bool debug = false;
+  int row_counter = 0;
+  if(debug){
+    cout << "Beginning to initialize the element in the 2d array" << endl;
+  }
+  while(*row_w != nullptr){
+    col_w = *row_w;
+    for(int i = 0; i<*sizes_p;i++){
+      if(debug){
+        cout << "placing " << init_item << " into " << i << " column, row  " << row_counter << endl;
+      }
+       *col_w = init_item;
+       col_w++;
+    } 
+    sizes_p++;
+    row_w++;
+    row_counter++; 
+  }
 }
 
 template<class T>
@@ -104,8 +175,45 @@ T* search_twod(T** twod, int* sizes, const T& key){
 
 template <class T>
 ostream& print_twod(T** twod, int* sizes, ostream& outs){
-
+  T** row_walker = twod;
+  T*column_walker;
+  int* sizes_w = sizes;
+  while(*row_walker!= nullptr){
+    column_walker = *row_walker;
+    print(column_walker, *sizes_w);
+    cout << endl;
+    row_walker++;
+    sizes_w++;
+  }
 }
 
+template <class T>
+void init_twod_debug(T** twod, int* sizes ){
+  T** row_w = twod;
+  T*col_w;
+  int*sizes_p = sizes;
+  const bool debug = false;
+  int row_counter = 0;
+  int j = 0;
+  if(debug){
+    cout << "Beginning to initialize the element in the 2d array" << endl;
+  }
+
+  while(*row_w != nullptr){
+    col_w = *row_w;
+    j = row_counter * 10;
+    for(int i = 0; i<*sizes_p;i++){
+      if(debug){
+        cout << "placing " << i<< " into " << i/10 << " column, row  " << row_counter << endl;
+      }
+       *col_w = j;
+       col_w++;
+       j++;
+    } 
+    sizes_p++;
+    row_w++;
+    row_counter++; 
+  } 
+}
 
 #endif
